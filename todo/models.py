7a0a3eabe75150a9afe91fa8c7fb058f03cde5a4
Naor Talmor <naortalmornew@gmail.com>
2020-04-08 18:36:52 +0300

    fetch category_id

--- todo/models.py
+++ todo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from categories.models import Category
 
 # Create your models here.
 class Todo(models.Model):
@@ -7,6 +8,7 @@ class Todo(models.Model):
     is_done = models.BooleanField(default=False)
     creation_date = models.DateField(auto_now_add=True)
     due_date = models.DateField(auto_now_add=False)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.header
--- todo/serializers.py
+++ todo/serializers.py
@@ -3,6 +3,11 @@ from .models import Todo
 
 
 class TodoSerializers(serializers.ModelSerializer):
+    category_id = serializers.SerializerMethodField('get_category_id')
+
+    def get_category_id(self, obj):
+        return obj.category_id.id
+
     class Meta:
         model = Todo
-        fields = ('id', 'header', 'description', 'is_done', 'creation_date', 'due_date')
+        fields = ('id', 'header', 'description', 'is_done', 'creation_date', 'due_date', 'category_id')
